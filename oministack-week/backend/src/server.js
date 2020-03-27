afc4604aa4cdf541607d4ff6b8b077ff5e71be42
johny anastacio <johnyanastaciods@gmail.com>
2020-03-27 17:26:53 -0300

    oministackweek11

---
+++ oministack-week/backend/src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const routes = require('./routes');
+
+app.use(express.json());
+app.use(routes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
+    console.log("SERVER IS RUNNING ON ::", PORT);
+});
