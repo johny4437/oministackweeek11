9f26c1c92449d91fc5af3d1ba36522438b50a6f7
johny anastacio <johnyanastaciods@gmail.com>
2020-03-28 20:00:55 -0300

    oministackweek11

--- oministack-week/backend/src/routes.js
+++ oministack-week/backend/src/routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const routes = express.Router();
+const crypto = require('crypto');
+const connection = require('./database/connection');
+const ongController = require('./controllers/ongController');
 
 
+routes.get('/ongs',ongController.list);
+routes.post('/ongs',ongController.create);
 
 module.exports = routes;
