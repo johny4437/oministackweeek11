742276dd54e4038018f66a27d7cc3977ddb6f464
johny anastacio <johnyanastaciods@gmail.com>
2020-03-29 18:53:45 -0300

    oministackweek11

--- oministack-week/backend/src/routes.js
+++ oministack-week/backend/src/routes.js
@@ -4,11 +4,19 @@ const crypto = require('crypto');
 const connection = require('./database/connection');
 const ongController = require('./controllers/ongController');
 const incidentController = require('./controllers/incidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
+routes.post('/login', SessionController.create);
 
 routes.get('/ongs',ongController.index);
 routes.post('/ongs',ongController.create);
 
+routes.get('/incidents',incidentController.index);
 routes.post('/incidents',incidentController.create);
+routes.delete('/incidents/:id',incidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
 
 module.exports = routes;
